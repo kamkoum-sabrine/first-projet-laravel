0e438d1e38735ae7b9f0827d137dd8acfce98b31
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-03-15 16:03:41 +0100

    update view

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
         <h1>Todo</h1>
         @foreach($posts as $post)
-            <p>{{ $posts->title }}</p>
+            <p>{{ $posts }}</p>
         @endforeach
     </body>
 </html>
