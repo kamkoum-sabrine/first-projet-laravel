8855b695c72624c923d6b17411ff4000cbcfd8d1
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-03-16 12:07:39 +0100

    correct path views

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,29 @@
+@extends('todos.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show todos</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('todos.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                {{ $todo->title }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Priority:</strong>
+                {{ $todo->priority }}
+            </div>
+        </div>
+    </div>
+@endsection
